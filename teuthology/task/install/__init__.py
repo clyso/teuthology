07ca2b775814229780ba0b0bd05b4baafb6f50f3
Kyr Shatskyy <kyrylo.shatskyy@clyso.com>
2025-10-16 00:41:21 +0200

    task/install: allow both list and dicts for extra_system_packages
    Fixes: https://tracker.ceph.com/issues/73527
    
    Signed-off-by: Kyr Shatskyy <kyrylo.shatskyy@clyso.com>

--- teuthology/task/install/__init__.py
+++ teuthology/task/install/__init__.py
@@ -417,6 +417,21 @@ def ceph_deploy_upgrade(ctx, config):
 ceph_deploy_upgrade.__doc__ = docstring_for_upgrade.format(
     cmd_parameter='ceph_deploy_upgrade')
 
+def _override_extra_system_packages(config, project, install_overrides):
+    teuthology.deep_merge(config, install_overrides.get(project, {}))
+    extra_overrides = install_overrides.get('extra_system_packages')
+    if extra_overrides:
+        extra = config.get('extra_system_packages')
+        if extra is None:
+            e = dict(deb=None, rpm=None)
+        elif isinstance(extra, list):
+            e = dict(deb=copy.deepcopy(extra), rpm=copy.deepcopy(extra))
+        elif isinstance(extra, dict):
+            e = copy.deepcopy(extra)
+        elif isinstance(extra, str):
+            e = dict(deb=[extra], rpm=[extra])
+
+        config['extra_system_packages'] = teuthology.deep_merge(e, extra_overrides)
 
 @contextlib.contextmanager
 def task(ctx, config):
@@ -568,15 +583,10 @@ def task(ctx, config):
     repos = None
 
     if overrides:
+        install_overrides = overrides.get('install', {})
+        log.debug('INSTALL overrides: %s' % install_overrides)
         try:
-            install_overrides = overrides.get('install', {})
-            log.debug('INSTALL overrides: %s' % install_overrides)
-            teuthology.deep_merge(config, install_overrides.get(project, {}))
-            overrides_extra_system_packages = install_overrides.get('extra_system_packages')
-            if overrides_extra_system_packages:
-                extra_system_packages = config.get('extra_system_packages')
-                config['extra_system_packages'] = teuthology.deep_merge(extra_system_packages, overrides_extra_system_packages)
-            repos = install_overrides.get('repos', None)
+            _override_extra_system_packages(config, project, install_overrides)
         except AssertionError:
             raise ConfigError(
                 "'install' task config and its overrides contain" \
@@ -584,6 +594,7 @@ def task(ctx, config):
                 "the configuration is of the same type (dict, list, etc.) " \
                 "in both the task definition and its overrides."
             )
+        repos = install_overrides.get('repos', None)
 
     log.debug('config %s' % config)
 
